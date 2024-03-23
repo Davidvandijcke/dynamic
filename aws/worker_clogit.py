119b805e97386248da780735c9be7f30012abce1
Ubuntu <ubuntu@ip-172-31-24-214.ec2.internal>
2024-03-23 03:34:07 +0000

    prepped clogit python file

--- aws/worker_clogit.py
+++ aws/worker_clogit.py
@@ -12,26 +12,27 @@ figs_dir = "s3://ipsos-dvd/dyn/results/figs/"
 
 
 if __name__ == "__main__":
-    workers = pd.read_csv(data_dyn + "dwomes_dense.csv")
+    workers = pd.read_csv("s3://ipsos-dvd/dyn/data/dwomes_dense_csv/part-00000-58a1eeb4-33ed-4cf2-b937-2a7e2bbf027b-c000.csv",
+                          on_bad_lines='skip', nrows=100000)
     workers['strata'] = workers['caid'] + workers['week'].astype(str)
 
     # create chain fixed effects
-    workers['chain_week'] = workers['chain'] + workers['week'].astype(str)
-    workers = pd.concat([workers, pd.get_dummies(workers['chain'], prefix="chain_")])
+    workers['chainweek'] = workers['chain'] + workers['week'].astype(str)
+    workers = pd.concat([workers, pd.get_dummies(workers['chainweek'], prefix="chain_", dtype="int")], axis=1)
 
     allcols = workers.columns
-    endog_names = [x for x in allcols if "salary" in x or "chain_" in x]
-    endog_names = endog_names + ['distance', 'distance_sq', 'total_caids', 'total_caids_sq']
-    exog_names = 'pr_work'
+    exog_names = [x for x in allcols if "salary" in x] # or "chain_" in x]
+    exog_names = exog_names + ['distance', 'distance_sq']
+    endog_names = 'pr_work'
 
     mdl = ConditionalLogit( # **kwargs are for LikelihoodModel Class
-        endog = np.array(workers[exog_names]),
-        exog = np.array(workers[endog_names]),
+        endog = np.array(workers[endog_names]),
+        exog = np.array(workers[exog_names]),
         groups = np.array(workers['strata'])
     )
 
-    res = mdl.fit(method="lbfgs", maxiter=5000)
+    res = mdl.fit(maxiter=100)
 
-    resdf = pd.DataFrame([(res.params[i], endog_names[i]) for i in range(len(endog_names))], columns = ['beta', 'name'])
+    resdf = pd.DataFrame([(res.params[i], exog_names[i]) for i in range(len(exog_names))], columns = ['beta', 'name'])
 
     resdf.to_csv(data_dyn + "clogit_out.csv", index=False)
